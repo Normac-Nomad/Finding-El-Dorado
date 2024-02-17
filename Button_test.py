80c88851a7e4896188d06c9842370ad59d72fdef
Normac-Nomad <normac.nomad@gmail.com>
2024-02-17 13:34:13 -0500

    Button test is working. Now to integrate it with the menu.

--- Button_test.py
+++ Button_test.py
@@ -18,31 +18,44 @@ CENTER_MENU_BUTTON_Y = (SCREEN_HEIGHT / 2) + MENU_BUTTON_Y_OFFSET
 WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
 pygame.display.set_caption('Button Demo') 
 
-start_img = pygame.image.load("assets/start.png").convert_alpha() 
-exit_img = pygame.image.load("assets/exit.png").convert_alpha() 
+start_img = pygame.image.load("assets/start.png").convert_alpha()  
+hover_start_img = pygame.image.load("assets/start_hover.png").convert_alpha() 
+exit_img = pygame.image.load("assets/exit.png").convert_alpha()  
+hover_exit_img = pygame.image.load("assets/exit_hover.png").convert_alpha() 
 
 class Button(): 
-    def __init__(self, x, y, image, scale): 
+    def __init__(self, x, y, image, hover, scale): 
         width = image.get_width() 
         height = image.get_height()
         self.image = pygame.transform.scale(image, (int(width* scale), int(height * scale))) 
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y) 
+        self.image_rect = self.image.get_rect()
+        self.image_rect.topleft = (x, y) 
+        self.clicked = False
 
+        hover_width = hover.get_width() 
+        hover_height = hover.get_height()
+        self.hover = pygame.transform.scale(hover, (int(hover_width* scale), int(hover_height * scale))) 
+        self.hover_rect = self.hover.get_rect()
+        self.hover_rect.topleft = (x, y)
+        
     def draw(self):  
         pos = pygame.mouse.get_pos() 
-        if self.rect.collidepoint(pos): 
-            #self.image = 
-            print("HOVER")
+        if (self.image_rect.collidepoint(pos)): 
+            WINDOW.blit(self.hover, (self.hover_rect.x, self.hover_rect.y)) 
+            if ((pygame.mouse.get_pressed()[0] == 1) and (self.clicked == False)): 
+                self.clicked = True
+        else: 
+            WINDOW.blit(self.image, (self.image_rect.x, self.image_rect.y))  
 
-        WINDOW.blit(self.image, (self.rect.x, self.rect.y))  
+        if (pygame.mouse.get_pressed()[0] == 0):
+            self.clicked = False
 
-start_button = Button(CENTER_MENU_BUTTON_X, CENTER_MENU_BUTTON_Y - MENU_BUTTON_DISTANCE, start_img, MENU_BUTTON_SCALE) 
-exit_button = Button(CENTER_MENU_BUTTON_X, CENTER_MENU_BUTTON_Y + MENU_BUTTON_DISTANCE, exit_img, MENU_BUTTON_SCALE)
+start_button = Button(CENTER_MENU_BUTTON_X, CENTER_MENU_BUTTON_Y - MENU_BUTTON_DISTANCE, start_img, hover_start_img, MENU_BUTTON_SCALE) 
+exit_button = Button(CENTER_MENU_BUTTON_X, CENTER_MENU_BUTTON_Y + MENU_BUTTON_DISTANCE, exit_img, hover_exit_img, MENU_BUTTON_SCALE)
 
 while True:  
      
-    WINDOW.fill((202, 228, 241)) 
+    WINDOW.fill((255, 220, 0)) 
 
     start_button.draw()
     exit_button.draw()
