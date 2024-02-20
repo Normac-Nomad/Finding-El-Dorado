8ec967b97ffa10f877fbd4bbb74f2245694b3b4a
Normac-Nomad <normac.nomad@gmail.com>
2024-02-20 12:24:01 -0500

    Version 0.1
    Most likely the final version of this project. The purpose of this project was learning the basics of python. I completed the tutorial, organized the code into separate files, and added mutiple features: sprint, character selection, level selection, user settings, multiple levels, vertical scrolling, etc.  There are still a few bugs and issues with collision, lack of try statements, and lack of proper levels/objects. However, the game runs well and the code is clean.

--- screens/levels.py
+++ screens/levels.py
@@ -123,10 +123,10 @@ def level_one():
     background, bg_image = get_background("Sky.png")  
     player = Player(100, 100, 50, 50)  
 
-    fire = Fire(100, WINDOW_HEIGHT - BLOCK_SIZE - 64, 16, 32)
+    fire = Fire(100, - 100, 16, 32)
     fire.on()
     floor = [Block(i * BLOCK_SIZE, WINDOW_HEIGHT - BLOCK_SIZE, BLOCK_SIZE) for i in range(-WINDOW_WIDTH // BLOCK_SIZE, (WINDOW_WIDTH * 2) // BLOCK_SIZE)] 
-    objects = [*floor, Block(0, WINDOW_HEIGHT - (BLOCK_SIZE * 2), BLOCK_SIZE), fire]  
+    objects = [*floor, fire]
 
     return (background, bg_image, player, fire, objects) 
 
@@ -144,7 +144,7 @@ def level_two():
     fire = Fire(100, WINDOW_HEIGHT - BLOCK_SIZE - 64, 16, 32)
     fire.on() 
     floor = [Block(i * BLOCK_SIZE, WINDOW_HEIGHT - BLOCK_SIZE, BLOCK_SIZE) for i in range(-WINDOW_WIDTH // BLOCK_SIZE, (WINDOW_WIDTH * 2) // BLOCK_SIZE)] 
-    objects = [*floor, Block(0, WINDOW_HEIGHT - (BLOCK_SIZE * 2), BLOCK_SIZE), fire]  
+    objects = [*floor, fire]  
 
     return (background, bg_image, player, fire, objects)
 
